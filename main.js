0395f157cfe0253ba9aa044062a1f499d9de7613
Pedro Martins <pedro@pedromartins.com.pt>
2024-12-04 13:04:52 +0000

    Tiddy up

--- main.js
+++ main.js
@@ -1,43 +1,47 @@
-// Tooltip
-const tooltipLinks = document.querySelectorAll(".iwTooltipLink");
+(function() {
 
-tooltipLinks.forEach(link => {
+	const tooltipLinks = document.querySelectorAll(".iwTooltipLink");
+	const input = document.querySelector("input");
+	const output = document.querySelector("output");
 
-	let id = Math.random().toString(36).substr(2, 9);
-	link.setAttribute('popovertarget', `tooltip-${id}`);
-	link.style.anchorName = `--tooltipAnchor-${id}`;
-	let tooltip;
+	// Tooltip
+	tooltipLinks.forEach(link => {
 
-	link.addEventListener("mouseenter", () => {
-		const content = link.getAttribute('data-tooltip');
+		let id = Math.random().toString(36).substr(2, 9);
+		let tooltip;
 
-		if (!content) return;
+		link.setAttribute('popovertarget', `tooltip-${id}`);
+		link.style.anchorName = `--tooltipAnchor-${id}`;
 
-		tooltip = document.createElement('div');
-		tooltip.id = `tooltip-${id}`;
-		tooltip.className = 'iwTooltip';
-		tooltip.setAttribute('popover', '');
-		tooltip.textContent = content;
-		tooltip.style.positionAnchor = `--tooltipAnchor-${id}`;
-		tooltip.style.transform = `translateX(-50%)`;
-		link.parentElement.appendChild(tooltip);
-		tooltip.showPopover();
+		link.addEventListener("mouseenter", () => {
+			const content = link.getAttribute('data-tooltip');
+
+			if (!content) return;
+
+			tooltip = document.createElement('div');
+			tooltip.id = `tooltip-${id}`;
+			tooltip.className = 'iwTooltip';
+			tooltip.setAttribute('popover', '');
+			tooltip.textContent = content;
+			tooltip.style.positionAnchor = `--tooltipAnchor-${id}`;
+			tooltip.style.transform = `translateX(-50%)`;
+			link.parentElement.appendChild(tooltip);
+			tooltip.showPopover();
+		});
+
+		link.addEventListener("mouseleave", () => {
+			if (tooltip) {
+				tooltip.hidePopover();
+				tooltip.remove();
+				tooltip = null;
+			}
+		});
 	});
 
-	link.addEventListener("mouseleave", () => {
-		if (tooltip) {
-            tooltip.hidePopover();
-            tooltip.remove();
-            tooltip = null;
-        }
+
+	//  Slider
+	input.addEventListener("input", (event) => {
+		output.innerText = `${input.value}`;
 	});
-});
 
-
-//  Slider
-const input = document.querySelector("input");
-const output = document.querySelector("output");
-
-input.addEventListener("input", (event) => {
-  output.innerText = `${input.value}`;
-});
+})();
